5ca99461a7e09498e6e14996ac1275315506fd7b
Ubuntu <ubuntu@ip-172-31-36-126.us-east-2.compute.internal>
2021-01-31 01:41:12 +0000

    Got API endpoint working for hero data. Working on dota.html

--- server_heroapi.py
+++ server_heroapi.py
@@ -3,19 +3,21 @@ import flask
 from flask import request, render_template
 import requests, json
 
-print(dota.getHeroData("invoker",2))
+#print(dota.getHeroData("invoker",2))
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] =  True
 
-@app.route('/', methods=['GET'])
+@app.route('/dota', methods=['GET'])
 def home():
-    return render_template('/index.html')
+    return render_template('dota.html')
 
 @app.route('/api/v1/dotahero', methods=['GET'])
 def respond():
     # here we want to get the value of user (i.e. ?hero=some-value&range=other-value)
     hero = request.args.get('hero')
-    timerange = request.args.get('range')
+    timerange = int(request.args.get('range'))
+    data = dota.getHeroData(hero,timerange)
+    return data
 
 app.run('0.0.0.0',port=5200)
